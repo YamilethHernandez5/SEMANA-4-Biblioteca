587c3f79668c32ca34f6683b135ce9cf9806e326
JulissaHdzClaros <calebyunissahdz@gmail.com>
2021-08-20 23:40:05 -0600

    Agregar archivos de proyecto.

---
+++ SEMANA 4 Biblioteca/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA_4_Biblioteca
+{
+    public class SampleCollection
+    {
+
+        public static void SortedListCollection()
+        {
+            
+            List<object> lst = new List<object>();
+            // capturar los elementos para almacenar dentro de una lista 
+
+            Console.WriteLine("Selecciona un elemeto: ");
+            Console.WriteLine("Blanco");
+            Console.WriteLine("Azul");
+            Console.WriteLine("Rojo");
+            Console.WriteLine("Verde");
+            Console.WriteLine("Morado");
+            Console.WriteLine("Rosa");
+            Console.WriteLine("naranja");
+            Console.WriteLine("amarillo");
+            Console.WriteLine("negro");
+            Console.WriteLine("gris");
+            Console.WriteLine("marrón");
+
+            var response = Console.ReadLine();
+
+            if (response == "Blanco")
+            {
+                Console.Write("La representación de el color es: Pureza, inocencia, paz y virtud");
+            }
+
+            if (response == "Azul")
+            {
+                Console.Write("La representación de el color es: Tranquilidad, frescura e inteligencia");
+            }
+
+            if (response == "Rojo")
+            {
+                Console.Write("La representación de el color es: Energía, furia, excitación y alegría ");
+            }
+
+            if (response == "Verde")
+            {
+                Console.Write("La representación de el color es: Esperanza, fertilidad y juventud");
+            }
+
+            if (response == "Morado")
+            {
+                Console.Write("La representación de el color es: Sofisticación, misterio, nostalgia, y espiritualidad");
+            }
+
+            if (response == "Rosa")
+            {
+                Console.Write("La representación de el color es: Dulzura, suavidad y seducción");
+            }
+
+            if (response == "Naranja")
+            {
+                Console.Write("La representación de el color es: Diversión, exótico o poco común");
+            }
+
+            if (response == "Amarillo")
+            {
+                Console.Write("La representación de el color es: Iluminación, felicidad, envidia, celos, incertidumbre y quietud");
+            }
+
+            if (response == "Negro")
+            {
+                Console.Write("La representación de el color es: Poder, muerte, elegancia, formalidad y sobriedad");
+            }
+
+            if (response == "Gris")
+            {
+                Console.Write("La representación de el color es: Paz, tenacidad, tranquilidad");
+            }
+
+            if (response == "Marrón")
+            {
+                Console.Write("La representación de el color es: Confortabilidad, humildad y equilibrio");
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ SENANA 4 Consola/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SEMANA_4_Biblioteca;
+
+
+
+namespace SENANA_4_Consola
+{
+     class Usolibreria
+     {
+          public static void SortedListCollection()
+          {
+            
+            Console.WriteLine("Seleccione el elemento a crear: ");
+            Console.WriteLine("1.SortedList ");
+            var response = Console.ReadLine();
+
+            if (response == "1")
+            {
+                SampleCollection.SortedListCollection();
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+          }
+            
+     } 
+ 
+}
+
+
+       
+          
+    
